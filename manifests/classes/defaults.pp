2fd5ac8b4b1219b1ef302ff4b32b8056f1c147ae
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2009-05-28 15:59:15 +0200

    Call records cleanup (by cjeanneret)

--- manifests/classes/defaults.pp
+++ manifests/classes/defaults.pp
@@ -5,4 +5,16 @@ class asterisk::defaults {
     ensure => present,
     source => "puppet:///asterisk/macro-dial-external.conf",
   }
+
+  package {"tmpreaper":
+    ensure => present,
+  }
+
+  cron { "remove call records older than 3 months":
+    command => "/usr/sbin/tmpreaper 90d /var/spool/asterisk/monitor",
+    user    => "root",
+    hour    => 3,
+    minute  => 0,
+    require => Package["tmpreaper"],
+  }
 }
