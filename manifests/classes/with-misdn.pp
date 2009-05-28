f079997f9142f2a611e99558721e630a25954136
Francois Deppierraz <francois.deppierraz@camptocamp.com>
2009-05-28 16:01:47 +0200

    Misdn support

---
+++ manifests/classes/with-misdn.pp
@@ -0,0 +1,23 @@
+class asterisk::with-misdn inherits asterisk::voip-only {
+
+  $password = "Zi2dae9a"
+
+  file {"/etc/asterisk/manager.conf.d/nagios.conf":
+    ensure => present,
+    mode => 0640,
+    owner => root,
+    group => asterisk,
+    content => template('asterisk/nagios-manager.conf.erb'),
+    notify => Exec["asterisk-reload"],
+  }
+
+  file {"/usr/lib/nagios/plugins/contrib/check_misdn.pl":
+    ensure => present,
+    mode => 0755,
+    owner => root,
+    group => root,
+    content => template('asterisk/check_misdn.pl.erb'),
+    require => File["/etc/asterisk/manager.conf.d/nagios.conf"],
+  }
+
+}
