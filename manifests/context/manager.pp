d45d6d8b5a2435d28c3200aa733df834678f7942
Maximilian Ronniger <mxr@rise-world.com>
2013-08-23 16:45:56 +0200

    add puppet asterisk manager class

---
+++ manifests/context/manager.pp
@@ -0,0 +1,16 @@
+define asterisk::context::manager (
+  $ensure = 'present',
+
+  $secret  = false,
+  $manager_name = 'manager',
+  $permit = '127.0.0.1/255.255.255.255',
+  $read = 'system,call',
+  $write = 'system,call') {
+
+  asterisk::dotd_file {"${name}.conf":
+    ensure  => $ensure,
+    dotd_dir => 'manager.conf.d',
+    content => template('asterisk/context/manager.erb'),
+    filename => "${name}.conf",
+  }
+}
