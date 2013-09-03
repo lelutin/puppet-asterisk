13e69fdf4af0958043d3a2d20e7d2cb341501494
Maximilian Ronniger <mxr@rise-world.com>
2013-09-03 09:57:06 +0200

    only include register files when they have been created

--- manifests/registry/sip.pp
+++ manifests/registry/sip.pp
@@ -11,6 +11,11 @@ define asterisk::registry::sip (
     fail('No value given for password: supplying a value for $authuser only makes sense when $password is set.')
   }
 
+  file_line{ 'include /etc/asterisk/sip.registry.d/*':
+      path => '/etc/asterisk/sip.conf',
+      line => "#include </etc/asterisk/sip.registry.d/*.conf>",
+  }
+
   asterisk::dotd_file { "${name}.conf":
     dotd_dir => 'sip.registry.d',
     content => template('asterisk/registry/sip.erb'),
