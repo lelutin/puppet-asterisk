8edc5596da7f56387f5f094fc8d407ac0c534411
Gabriel Filion <gabster@lelutin.ca>
2014-03-09 20:37:54 -0400

    Bring back class parameters on main class.
    Moving logic for these into a ::params class was the right way to go,
    but removing them from the main class and spreading them into subclasses
    was not the best choice.
    
    Best practice wrt class parameters is to only have them in the main
    class whenever possible.
    
    Signed-off-by: Gabriel Filion <gabster@lelutin.ca>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,12 @@
 # type that installes asterisk base packages.
-class asterisk {
-  require asterisk::params
+class asterisk (
+  $manage_service = $asterisk::params::manage_service,
+  $confdir        = $asterisk::params::confdir,
+  $iax_options    = $asterisk::params::iax_options,
+  $sip_options    = $asterisk::params::sip_options,
+  $package_name   = $asterisk::params::package_name,
+  $service_name   = $asterisk::params::service_name
+) inherits asterisk::params {
 
   anchor { 'begin': }
   class { 'asterisk::install': } ->
