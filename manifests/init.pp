eb78fa8ebadc5c23fe059caf6137e937d6570bd7
Gabriel Filion <gabster@lelutin.ca>
2017-06-26 16:26:34 -0400

    better arrow alignment for ordering
    apparently I've been using an antipattern in puppetlabs's style guide.
    
    placing the arrows at the beginning of the line makes it more obvious
    that there is an ordering going on. otherwise you need to spot the arrow
    at the end of the line.

--- manifests/init.pp
+++ manifests/init.pp
@@ -90,11 +90,11 @@ class asterisk (
   # Anchor this as per #8040 - this ensures that classes won't float off and
   # mess everything up. You can read about this at:
   # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
-  anchor { 'asterisk::begin': } ->
-  class { 'asterisk::install': } ->
-  class { 'asterisk::config': } ~>
-  class { 'asterisk::service': } ->
-  anchor { 'asterisk::end': }
+  anchor { 'asterisk::begin': }
+  -> class { 'asterisk::install': }
+  -> class { 'asterisk::config': }
+  ~> class { 'asterisk::service': }
+  -> anchor { 'asterisk::end': }
 
 }
 
