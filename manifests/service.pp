f45e362d392dbec3295ec11c6d4c436ea14cbf60
Gabriel Filion <gabster@lelutin.ca>
2018-12-14 08:39:53 -0500

    stop managing service when $manage_service is set to false
    when users tell the module to avoid managing the service ... we
    shouldn't go about managing it and disabling it. that doesn't make any
    sense.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,20 +1,11 @@
 # Ensure the Asterisk service is running.
 #
-# FIXME: why are we disabling the service when $manage_service is set to false?
-# this does not make any sense.
-#
 class asterisk::service {
-  $manage_service = $asterisk::manage_service
 
-  if $manage_service {
-    service {$asterisk::service_name:
+  if $asterisk::manage_service {
+    service { $asterisk::service_name:
       ensure  => running,
     }
-  } else {
-    service {$asterisk::service_name:
-      enable  => false,
-    }
-
   }
 
 }
