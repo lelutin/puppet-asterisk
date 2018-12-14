387e1d7e961b36950805038a8552342a8f6cb8de
Gabriel Filion <gabster@lelutin.ca>
2018-12-14 07:56:40 -0500

    start adding tests to the module
    this first test is not super significant, but we'll get to write better
    ones.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 describe 'asterisk' do
+  let(:title) { 'asterisk' }
+  let(:facts) { {
+    :operatingsystem => 'Debian',
+  } }
 
   context 'with defaults for all parameters' do
     it { should contain_class('asterisk') }
