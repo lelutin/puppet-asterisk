d5bd6445b0dac9e6fdd6e7ab7631befd50bdc108
Gabriel Filion <gabster@lelutin.ca>
2018-12-15 09:15:38 -0500

    create a new test that installs the simplest example of all resources
    This file is meant to be a quick and dirty integration test

---
+++ tests/one_of_all.pp
@@ -0,0 +1,57 @@
+# This test file is meant to be a quick and dirty acceptance test for the main
+# class without arguments and the most simple case for all of the defined
+# types. Its purpose is to make it easy to see if something got broken somehow.
+
+include asterisk
+
+asterisk::agent { 'smith':
+  ext        => '7000',
+  password   => Sensitive.new('misssteranderson'),
+  agent_name => 'smith',
+}
+
+asterisk::extensions { 'press_one':
+  content => 'exten => 500,1,Answer',
+}
+
+asterisk::feature { 'exclusivity':
+  options => {
+    phone => '555-555-5555',
+  }
+}
+
+asterisk::feature::applicationmap { 'bananaphone':
+  feature => 'ringtone',
+  value   => 'ringringringringring',
+}
+
+asterisk::iax { 'meetingroom':
+  content => "type=user\ncontext=press_one",
+}
+
+asterisk::language { 'fr-armelle': }
+
+asterisk::manager { 'michael':
+  secret => Sensitive.new('notagoodmanager'),
+}
+
+asterisk::queue { 'tail': }
+
+asterisk::sip { 'skippy': }
+
+asterisk::voicemail { '200':
+  context  => 'press_one',
+  password => Sensitive.new('whoyougonnacall'),
+}
+
+asterisk::registry::iax { 'providerX':
+  server   => 'foo.local',
+  user     => 'carolina',
+  password => Sensitive.new('thisisapasswordwhatyoulookingat'),
+}
+
+asterisk::registry::sip { 'myphone':
+  server => 'sipphone.local',
+  user   => 'line1',
+}
+
