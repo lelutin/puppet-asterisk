0924e429ec4d721433c0962a12e1ea8df4e8935d
Gabriel Filion <gabster@lelutin.ca>
2018-12-16 01:15:00 -0500

    add some more objectives wrt extensions

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,8 @@
 #
 # @todo Purge unmanaged configs by default. Add parameter to disable purging.
 # @todo Add hash-based params that can be used to create a set of resources for each type with create_resource. This can be useful for pushing data out to hiera.
+# @todo make it possible to manage dialplan with the two other methods (e.g. AEL and Lua)
+# @todo manage options for the `[globals]` section of extensions.conf
 #
 # @param manage_service
 #   Set this to false to avoid managing the asterisk service. By default puppet
