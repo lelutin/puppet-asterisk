b8bd74bbe901db07e5096b7dad61cc63e66ad468
Gabriel Filion <gabster@lelutin.ca>
2023-03-17 17:57:39 -0400

    simplify spec_helper
    also use rspec-puppet-facts for the ease of writing the tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,2 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet'
-
-fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
-
-RSpec.configure do |c|
-  c.module_path     = File.join(fixture_path, 'modules')
-  c.manifest_dir    = File.join(fixture_path, 'manifests')
-  c.manifest        = File.join(fixture_path, 'manifests', 'site.pp')
-  c.environmentpath = File.join(Dir.pwd, 'spec')
-end
+require 'rspec-puppet-facts'
