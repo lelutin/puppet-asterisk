9e27d6a80ef5116d0cfdc094dddf09a60138e1ab
Gabriel Filion <gabster@lelutin.ca>
2023-04-16 18:52:44 -0400

    Use only one template for all config files
    This has been on my mind for a long time. All config files in asterisk
    are very similar. They're kind of ini-ish but not exactly. But the
    differences between the files are very minor. So keeping around one
    template for each different asterisk configuration file adds more files
    that need to be managed individually.
    
    This deduplicates what's used for generating configuration.
    
    This change also replaces old .erb files with a .epp template.
    
    It also changes some tiny details:
    
     * includes for config files inside .d directories is now relative
       instead of absolute, and they force the .conf extension
     * directories and config files are now owned by the asterisk user. I
       have no idea why they were set to be owned by root in the first
       place. This change minimizes the number of change lines when puppet
       first configures the service from scratch.

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,70 +8,189 @@ class asterisk::config {
 
   assert_private()
 
-  $iax_general = $asterisk::iax_general
-  asterisk::dotd { '/etc/asterisk/iax':
-    additional_paths => ['/etc/asterisk/iax.registry.d'],
-    content          => template('asterisk/iax.conf.erb'),
+  $cf_with_directory = [
+    'iax',
+    'sip',
+    'voicemail',
+    'extensions',
+    'agents',
+    'features',
+    'queues',
+    'manager',
+  ]
+  $directories = $cf_with_directory + ['iax.registry', 'sip.registry']
+
+  $directories.each |String $dir| {
+    file { "${asterisk::confdir}/${dir}.d" :
+      ensure  => directory,
+      owner   => 'asterisk',
+      group   => 'asterisk',
+      mode    => '0750',
+    }
+
+    # lint:ignore:140chars
+    # Avoid error messages
+    # [Nov 19 16:09:48] ERROR[3364] config.c: *********************************************************
+    # [Nov 19 16:09:48] ERROR[3364] config.c: *********** YOU SHOULD REALLY READ THIS ERROR ***********
+    # [Nov 19 16:09:48] ERROR[3364] config.c: Future versions of Asterisk will treat a #include of a file that does not exist as an error, and will fail to load that configuration file.  Please ensure that the file '/etc/asterisk/iax.conf.d/*.conf' exists, even if it is empty.
+    # lint:endignore
+    file { "${asterisk::confdir}/${dir}.d/null.conf":
+      ensure  => present,
+      owner   => 'asterisk',
+      group   => 'asterisk',
+      mode    => '0640',
+      content => '',
+    }
   }
 
-  $sip_general = $asterisk::sip_general
-  asterisk::dotd { '/etc/asterisk/sip':
-    additional_paths => ['/etc/asterisk/sip.registry.d'],
-    content          => template('asterisk/sip.conf.erb'),
+  # Options for the logfiles configuration file need some special per-line
+  # formatting. This creates one formatted string value for each config line
+  # and returns a hash with the string values instead of the sub-hash.
+  $formatted_logfiles = $asterisk::log_files.map |$section, $options| {
+    if $options['formatter'] !~ Undef {
+      $formatter = "[${options['formatter']}]"
+    }
+    else {
+      $formatter = ""
+    }
+
+    $return_value = {
+      $section => "${formatter}${options['levels'].join(',')}"
+    }
+  }.reduce( {} ) |$result, $value| { $result + $value }
+
+  $configs_with_registry = ['iax', 'sip']
+  $config_sections = {
+    iax => {
+      general => {
+        delimiter => '=',
+        options   => $asterisk::iax_general,
+      },
+    },
+    sip => {
+      general => {
+        delimiter => '=',
+        options   => $asterisk::sip_general,
+      },
+    },
+    voicemail => {
+      general => {
+        delimiter => '=',
+        options   => $asterisk::voicemail_general,
+      },
+    },
+    extensions => {
+      general => {
+        delimiter => '=',
+        options   => $asterisk::extensions_general,
+      },
+      globals => {
+        delimiter => '=',
+        options   => $asterisk::extensions_globals,
+      },
+    },
+    agents => {
+      general => {
+        delimiter => '=',
+        options   => { multiplelogin => $asterisk::agents_multiplelogin },
+      },
+      agents => {
+        delimiter => '=',
+        options   => $asterisk::agents_global,
+      },
+    },
+    features => {
+      general => {
+        delimiter => '=',
+        options   => $asterisk::features_general,
+      },
+      featuremap => {
+        delimiter => ' => ',
+        options   => $asterisk::features_featuremap,
+      },
+      applicationmap => {
+        delimiter => ' => ',
+        options   => $asterisk::features_applicationmap,
+      },
+    },
+    queues => {
+      general => {
+        delimiter => '=',
+        options   => $asterisk::queues_general,
+      },
+    },
+    logger => {
+      general => {
+        delimiter => '=',
+        options   => $asterisk::logger_general,
+      },
+      logfiles => {
+        delimiter => ' => ',
+        options   => $formatted_logfiles,
+      },
+    },
+    manager => {
+      general => {
+        delimiter => '=',
+        options   => {
+          enabled  => $asterisk::manager_enable,
+          port     => $asterisk::manager_port,
+          bindaddr => $asterisk::manager_bindaddr,
+        },
+      },
+    },
+    modules => {
+      modules => {
+        delimiter         => ' => ',
+        # Asterisk's config format is really weird. It looks like ini file but
+        # it's not exactly that, and it can sometimes (like in this case) mix
+        # up two types of option delimiters in the same section. The value in
+        # oddball_options use '=' instead of ' => '
+        oddball_delimiter => '=',
+        oddball_options   => {
+          autoload => bool2str($asterisk::modules_autoload, 'yes', 'no'),
+        },
+        options           => {
+          load     => $asterisk::modules_load,
+          noload   => $asterisk::modules_noload,
+        },
+      },
+      global => {
+        delimiter => '=',
+        options   => $asterisk::modules_global,
+      },
+    },
   }
 
-  $voicemail_general = $asterisk::voicemail_general
-  asterisk::dotd { '/etc/asterisk/voicemail':
-    content => template('asterisk/voicemail.conf.erb'),
-  }
+  $config_files = $cf_with_directory + [
+    'logger',
+    'modules',
+  ]
+  $config_files.each |String $filename| {
+    if $filename in $cf_with_directory {
+      $main_cf_dir = ["${filename}.d"]
+    }
+    else {
+      $main_cf_dir = []
+    }
 
-  $ext_context = {
-    general => $asterisk::extensions_general,
-    globals => $asterisk::extensions_globals,
-  }
-  asterisk::dotd { '/etc/asterisk/extensions':
-    content => epp('asterisk/extensions.conf.epp', $ext_context),
-  }
+    if $filename in $configs_with_registry {
+      $cf_directories = $main_cf_dir + ["${filename}.registry.d"]
+    } else {
+      $cf_directories = $main_cf_dir
+    }
 
-  $agents_multiplelogin = bool2str($asterisk::agents_multiplelogin, 'yes', 'no')
-  asterisk::dotd { '/etc/asterisk/agents':
-    content => template('asterisk/agents.conf.erb'),
+    $template_variables = {
+      config_name  => $filename,
+      include_dirs => $cf_directories,
+      sections     => $config_sections[$filename],
+    }
+    file { "${asterisk::confdir}/${filename}.conf":
+      ensure  => present,
+      owner   => 'asterisk',
+      group   => 'asterisk',
+      mode    => '0640',
+      content => epp('asterisk/config_file.conf.epp', $template_variables),
+    }
   }
-
-  $features_general = $asterisk::features_general
-  asterisk::dotd { '/etc/asterisk/features':
-    content          => template('asterisk/features.conf.erb'),
-  }
-
-  $queues_general = $asterisk::queues_general
-  asterisk::dotd { '/etc/asterisk/queues':
-    content => template('asterisk/queues.conf.erb'),
-  }
-
-  $logger_general = $asterisk::logger_general
-  $log_files = $asterisk::log_files
-  file { '/etc/asterisk/logger.conf' :
-    ensure  => present,
-    content => template('asterisk/logger.conf.erb'),
-    owner   => 'root',
-    group   => 'asterisk',
-    mode    => '0640',
-  }
-
-  $manager_enable = bool2str($asterisk::manager_enable, 'yes', 'no')
-  $manager_port = $asterisk::manager_port
-  $manager_bindaddr = $asterisk::manager_bindaddr
-  asterisk::dotd { '/etc/asterisk/manager':
-    content => template('asterisk/manager.conf.erb'),
-  }
-
-  $modules_autoload = bool2str($asterisk::modules_autoload, 'yes', 'no')
-  file { '/etc/asterisk/modules.conf' :
-    ensure  => present,
-    content => template('asterisk/modules.conf.erb'),
-    owner   => 'root',
-    group   => 'asterisk',
-    mode    => '0640',
-  }
-
 }
